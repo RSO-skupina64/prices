905186981b951f5e62fcf6834d95a68b85985398
asmaljcelj <aljaz.smaljcelj7@gmail.com>
2022-12-26 23:49:10 +0100

    [#6] added configuration from environment variables and config server as well as config file
    - added k8s deployment file
    - added new maven dependencies for h2 and spring-cloud-starter-config

---
+++ src/main/java/com/rso/microservice/config/DataSourceConfig.java
@@ -0,0 +1,36 @@
+package com.rso.microservice.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    private static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
+
+    private final Environment environment;
+
+    public DataSourceConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        String url = environment.getProperty("spring.datasource.url");
+        String username = environment.getProperty("spring.datasource.username");
+        String password = environment.getProperty("spring.datasource.password");
+        log.info("Using database URL: {}", url);
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+}
