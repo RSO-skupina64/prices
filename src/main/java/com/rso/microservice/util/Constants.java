7a568a34437a256dd6f94029fa479408aecbfe81
asmaljcelj <aljaz.smaljcelj7@gmail.com>
2023-01-02 16:09:07 +0100

    [#9] implemented logging

---
+++ src/main/java/com/rso/microservice/util/Constants.java
@@ -0,0 +1,9 @@
+package com.rso.microservice.util;
+
+public class Constants {
+
+    public static final String MDC_NAME = "microservice-name";
+    public static final String MDC_VERSION = "microservice-version";
+    public static final String MDC_REQUEST_ID = "microservice-request-id";
+
+}
